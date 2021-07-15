11ec7798abe4338c6643a01e68e65ebf8975996c
m32795434 <manubravard@gmail.com>
2021-07-15 20:23:26 -0300

    initial+creamos title.component+llevando y trayendo datos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gatitos';
+  subtitle = "todo lo que necesitas saber";
 }
---
+++ src/app/components/title/title.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-title',//asi lo vamos a llamar para usar en html
+  templateUrl: './title.component.html',
+  styleUrls: ['./title.component.css']
+})
+export class TitleComponent implements OnInit {
+
+  constructor() { }
+// propiedades de la clase
+//title = "titulo del h1";
+//subtitle = "subtitle del h1";
+// valor predeterminado o indicador de opcional, sino se rompe en el @input!
+@Input() title?: string;
+@Input() subtitle?: string;
+
+  ngOnInit(): void {
+  }
+
+}
