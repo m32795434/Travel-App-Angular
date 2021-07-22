359da5e883d1e3dbfd970dc7ec2927e0b61cecad
m32795434 <manubravard@gmail.com>
2021-07-22 18:30:15 -0300

    one comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// importamos HttpClientModule !
 import { HttpClientModule } from '@angular/common/http';
 
 
